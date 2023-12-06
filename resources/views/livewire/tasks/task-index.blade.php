bea7dbd0d40b78f1bf8534a7c45b95fd35e9350b
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-12-06 11:36:13 +0100

    feat: made use of the wire object to check numbers of characters in the input field

--- resources/views/livewire/tasks/task-index.blade.php
+++ resources/views/livewire/tasks/task-index.blade.php
@@ -1,12 +1,16 @@
 <div>
 
     <div class="text-center pb-10">
-        
+
         <input
             class="rounded-md w-1/5 border-0 py-2 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
             type="text"
             wire:model="name"
         />
+
+        <span class="absolute -ml-6 mt-2">
+            <span x-text="$wire.name.length"></span>
+        </span>
 
         <x-button :button="$button" />
 
